4753f11be01ad3f485535d592c39620e6c1fd4a1
Shyla <shailajasrinivas91@gmail.com>
2022-12-25 15:24:29 -0500

    app.js completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 
 function App() {
@@ -20,7 +21,12 @@ function App() {
   return (
     <main>
       <section className="menu section">
-
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
