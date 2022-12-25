1e73f695dc557ede640d937bb147c46126512a7b
Shyla <shailajasrinivas91@gmail.com>
2022-12-25 14:48:42 -0500

    menu component completed

--- src/Menu.js
+++ src/Menu.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const{id, title, img, desc, price} = menuItem;
+        return(
+          <article key={id} className="menu-item">
+          <img src={img} alt={title} className='photo' />
+          <div className="item-info">
+            <header>
+              <h4>{title}</h4>
+              <h4 className="price">${price}</h4>
+            </header>
+            <p className="item-text">{desc}</p>
+          </div>
+
+          </article>
+        )
+      }
+      )}
+      
+    </div>
   )
 }
 
