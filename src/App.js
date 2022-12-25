348de1010ded627e2b3dcfcca210e51227f8b4b3
Shyla <shailajasrinivas91@gmail.com>
2022-12-25 15:44:11 -0500

    debugging

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
-
 const allCategories = ['all', ...new Set(items.map((item) => item.category))];
-
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
-  
+
   const filterItems = (category) => {
     if (category === 'all') {
       setMenuItems(items);
@@ -18,6 +16,7 @@ function App() {
     const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);
   };
+
   return (
     <main>
       <section className="menu section">
